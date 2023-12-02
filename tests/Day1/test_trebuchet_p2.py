660d318275c0583444f9872424947af1f50282dd
Hsitirb <hsitirb1@gmail.com>
2023-12-02 20:27:05 +0000

    Day 1 Problem 2: Add tests for problem 2

---
+++ tests/Day1/test_trebuchet_p2.py
@@ -0,0 +1,36 @@
+import pytest
+from Day1.trebuchet import line_value, lines_result
+
+lines = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+lines_vals = (
+    ("two1nine", 29),
+    ("eightwothree", 83),
+    ("abcone2threexyz", 13),
+    ("xtwone3four", 24),
+    ("4nineeightseven2", 42),
+    ("zoneight234", 14),
+    ("7pqrstsixteen", 76),
+)
+
+@pytest.mark.parametrize(
+    "input_line,expected",
+    lines_vals
+)
+def test_line_value(input_line, expected):
+    assert line_value(input_line) == expected
+
+@pytest.mark.parametrize(
+    "input_lines,expected",
+    (
+        (lines, 281),
+    )
+)
+def test_total_value(input_lines, expected):
+    assert lines_result(input_lines) == expected
