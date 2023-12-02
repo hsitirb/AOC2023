ded73b19ba90e4e4ebd5a30f31e5807984b05606
Hsitirb <hsitirb1@gmail.com>
2023-12-02 19:58:11 +0000

    Day 1 Puzzle 1: Add cli run to calculate result from puzzle input

--- Day1/trebuchet.py
+++ Day1/trebuchet.py
@@ -5,3 +5,9 @@ def line_value(line):
 
 def lines_result(lines):
     return sum(line_value(line) for line in lines.splitlines())
+
+
+if __name__ == "__main__":
+    with open("Day1/puzzle_input.txt") as file:
+        lines = file.read()
+    print(lines_result(lines))
