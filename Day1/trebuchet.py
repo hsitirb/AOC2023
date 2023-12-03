ca3a694a40e0e1f089f52e239101cfa4383df5b9
Hsitirb <hsitirb1@gmail.com>
2023-12-03 17:16:43 +0000

    Day 1 Problem 2: Implementation to meet Day 2 tests

--- Day1/trebuchet.py
+++ Day1/trebuchet.py
@@ -1,4 +1,15 @@
 def line_value(line):
+    num_dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+    idx = 0
+    while idx < len(line):
+        for key in num_dict.keys():
+            if line[idx:].startswith(key):
+                line = line[:idx] + num_dict[key] + line[idx + len(key):]
+                idx += 1
+                break
+        else:
+            idx += 1
+
     line_nums = list(val for val in line if val.isnumeric())
     num = line_nums[0] + line_nums[-1]
     return int(num)
