0f2bb7b7a0cb823e0b5edddf5dcd62bf461f28bd
hsitirb <114746916+hsitirb@users.noreply.github.com>
2023-12-04 23:34:48 +0000

    Update game.py

--- Day2/game.py
+++ Day2/game.py
@@ -6,4 +6,29 @@ class Bag:
     green: int
     blue: int
     
-class Game: ...
+class Bag:
+    def __init__(self, red: int, blue: int, green: int):
+        self.red = red
+        self.blue = blue
+        self.green = green
+
+    def is_possible(self, game: Game) -> bool:
+          return (
+             red <= game.min_red and
+             blue <= game.min_blue and
+             green <= game.min_green
+         )
+
+class Game:
+    def __init__(self, game_spec: str):
+        self.rounds = []
+        game_info, round_info = line.split(":")
+        _, self.id = game_info.strip().split()
+        self.rounds = [Round(round_spec) for round_spec in round_info.split(";")
+
+class Round:
+    def __init__(self, round_spec: str):
+        for section in round_spec.split(","):
+            num, colour = section.strip().split()
+            assert colour in ("red", "green", "blue")
+            set_attr(self, colour, int(num))
