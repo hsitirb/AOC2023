ca2441f1a7c19447fa82339803a8c8ec62dae183
Hsitirb <hsitirb1@gmail.com>
2023-12-06 23:05:45 +0000

    Day 4 Problem 1: Add implementation to match (corrected) tests

--- Day4/cards.py
+++ Day4/cards.py
@@ -1,1 +1,14 @@
-class Card: ...
+class Card:
+    def __init__(self, card_spec):
+        self.card_id, rest = card_spec.split(":")
+        winning_numbers, selected_numbers = rest.split("|")
+        self.winning_numbers = [int(num.strip()) for num in winning_numbers.split()]
+        self.selected_numbers = [int(num.strip()) for num in selected_numbers.split()]
+
+    @property
+    def value(self):
+        match_set = set(self.winning_numbers) & set(self.selected_numbers)
+        if len(match_set) == 0:
+            return 0
+        else:
+            return 2 ** (len(match_set) - 1) 
