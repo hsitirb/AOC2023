bab81fc111c82b64bd31eaf8c59a005ae0ceecc6
Hsitirb <hsitirb1@gmail.com>
2023-12-06 18:07:51 +0000

    Day 3 Problem 1: Add implementation to meet (modified) tests

--- Day3/schematic.py
+++ Day3/schematic.py
@@ -1,2 +1,52 @@
+from dataclasses import dataclass
+
+@dataclass
+class Loc:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Loc(self.x + other.x, self.y + other.y)
+
+    def __hash__(self):
+        return self.x * 10000 + self.y
+
 class Schematic:
-    ...
+    def __init__(self, schematic: str):
+        schematic_part_nos = {}
+        schematic_symbols = []
+        self.valid_partnos = set()
+        for row, line in enumerate(schematic.splitlines()):
+            state = None
+            for col, symbol in enumerate(line):
+                if state == "partno":
+                    if not symbol.isnumeric():
+                        for loc_col in range(start_col, col):
+                            schematic_part_nos[Loc(row, loc_col)] = partno
+                        state = None
+                        partno = None
+                        start_col = None
+                    else:
+                        partno += symbol
+                if state is None:
+                    if symbol != "." and not symbol.isnumeric():
+                        schematic_symbols.append(Loc(row, col))
+                    elif symbol.isnumeric():
+                        state = "partno"
+                        partno = symbol
+                        start_col = col
+            for symbol_loc in schematic_symbols:
+                for offset in (
+                    # fmt: off
+                    Loc(-1, -1), Loc( 0, -1), Loc( 1, -1),
+                    Loc(-1,  0),              Loc( 1,  0),
+                    Loc(-1,  1), Loc( 0,  1), Loc( 1,  1)
+                    # fmt: on
+                ):
+                    offset_symbol_loc = symbol_loc + offset
+                    if offset_symbol_loc in schematic_part_nos:
+                        self.valid_partnos.add(int(schematic_part_nos[offset_symbol_loc]))
+
+    @property
+    def parts_list(self):
+        return list(self.valid_partnos)
--- tests/Day3/test_day3_part1.py
+++ tests/Day3/test_day3_part1.py
@@ -12,8 +12,15 @@ test_input = """467..114..
 ...$.*....
 .664.598.."""
 
-def test_part_number_list():
-    assert set(Schematic(test_input).parts_list) == set("467", "35", "633", "617", "592", "755", "664", "598")
+@pytest.mark.parametrize(
+        "test_input, expected",
+        (
+            (test_input, set([467, 35, 633, 617, 592, 755, 664, 598])),
+            ("617*......", set([617])),
+        )
+)
+def test_part_number_list(test_input, expected):
+    assert set(Schematic(test_input).parts_list) == expected
 
 def test_part_number_sum():
-    assert sum(Schematic(test_input)) == 4361
+    assert sum(Schematic(test_input).parts_list) == 4361
