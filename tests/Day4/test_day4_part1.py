648161763700dbc28d7b8d4779cbd5a3f5752967
Hsitirb <hsitirb1@gmail.com>
2023-12-06 22:52:30 +0000

    Day 4 Problem 1: Add tests for part1

---
+++ tests/Day4/test_day4_part1.py
@@ -0,0 +1,33 @@
+import pytest
+from Day4.cards import Card
+
+test_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
+@pytest.mark.parametrize(
+    "card_spec, expected",
+    (
+        ("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 2),
+        ("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
+        ("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 1),
+        ("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 0),
+        ("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0),
+        ("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0),
+    )
+)
+def test_card_value(card_spec, expected):
+    assert Card(card_spec) == expected
+
+@pytest.mark.parametrize(
+        "card_specs, expected",
+        (
+            (test_input, 13),
+        )
+)
+def test_card_total(card_specs, expected):
+    assert sum(Card(card_spec) for card_spec in card_specs.splitlines()) == expected
+    
