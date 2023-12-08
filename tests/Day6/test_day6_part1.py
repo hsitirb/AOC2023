e1917f0e92af6fdc06ea8cb0cf9b9834eeaaf64f
Hsitirb <hsitirb1@gmail.com>
2023-12-08 17:47:31 +0000

    Day 6 Problem 1: Minimal tests

---
+++ tests/Day6/test_day6_part1.py
@@ -0,0 +1,27 @@
+import pytest
+from Day6.boatrace import BoatRace
+
+test_input = """Time:      7  15   30
+Distance:  9  40  200"""
+
+
+@pytest.mark.parametrize(
+        "test_input, expected",
+        (
+            (test_input, 288),
+        ),
+    )
+def test_solution(test_input, expected):
+    assert BoatRace(test_input).calculate_best() == expected
+
+
+@pytest.mark.parametrize(
+    "race_spec, race_no, expected",
+    (
+        (test_input, 1, 4),
+        (test_input, 2, 8),
+        (test_input, 3, 11),
+    ),
+)
+def test_race_wins(race_spec, race_no, expected):
+    assert BoatRace(race_spec).wins_for_race_no(race_no) == expected
