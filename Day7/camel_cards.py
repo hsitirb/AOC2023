4094052821306e87fbe14504cca24cc5fcc9f902
Hsitirb <hsitirb1@gmail.com>
2023-12-16 12:42:58 +0000

    Day 7 Problem 1: Initial tests for Camel Cards

--- Day7/camel_cards.py
+++ Day7/camel_cards.py
@@ -1,1 +1,59 @@
-class CamelCards: ...
+from dataclasses import dataclass
+from enum import Enum
+
+Card = Enum("Card", ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"])
+Type = Enum("Type", ["FIVE_OF_A_KIND", "FOUR_OF_A_KIND", "FULL_HOUSE", "THREE_OF_A_KIND", "TWO_PAIR", "ONE_PAIR", "HIGH_CARD"])
+
+
+@dataclass
+class Hand:
+    hand: str
+    bid: int
+
+    def type(self):
+        typedict = {
+            Type.FIVE_OF_A_KIND: self.is_five_of_a_kind,
+            Type.FOUR_OF_A_KIND: self.is_four_of_a_kind,
+            Type.FULL_HOUSE: self.is_full_house,
+            Type.THREE_OF_A_KIND: self.is_three_of_a_kind,
+            Type.TWO_PAIR: self.is_two_pair,
+            Type.ONE_PAIR: self.is_one_pair,
+            Type.HIGH_CARD: self.is_high_card,
+        }
+        for hand_type, determiner in typedict.items():
+            if determiner():
+                return hand_type
+
+
+    def is_five_of_a_kind(self):
+        return all(Card(self.hand[0]) == Card(card) for card in self.hand)
+
+    def is_four_of_a_kind(self):
+        return (
+            self.hand.count(self.hand[0] == 4) or 
+            self.hand.count(self.hand[1] == 4)
+        )
+
+    def is_full_house(self):
+        return sum([self.hand.count(self.hand[idx]) for idx in range(len(self.hand))]) == 13
+
+    def is_three_of_a_kind(self):
+        return (
+            self.hand.count(self.hand[0] == 3) or 
+            self.hand.count(self.hand[1] == 3) or 
+            self.hand.count(self.hand[2] == 3)
+        )
+
+    def is_two_pair(self):
+        return sum([self.hand.count(self.hand[idx]) for idx in range(len(self.hand))]) == 9
+
+    def is_one_pair(self):
+        return sum([self.hand.count(self.hand[idx]) for idx in range(len(self.hand))]) == 7
+
+class CamelCards:
+    def __init__(self, spec):
+        self.hands = []
+        for line in spec.splitlines():
+            hand, bid = line.split()
+            self.hands.append(Hand(hand, int(bid)))
+            
--- tests/Day7/test_day7_part1.py
+++ tests/Day7/test_day7_part1.py
@@ -1,5 +1,5 @@
 import pytest
-from Day7.camel_cards import CamelCards
+from Day7.camel_cards import CamelCards, Type
 
 test_input = """32T3K 765
 T55J5 684
@@ -15,3 +15,20 @@ QQQJA 483"""
 )
 def test_total_winnings(hand_spec, expected):
     assert CamelCards(hand_spec).total_winnings() == expected
+
+@pytest.mark.parametrize(
+    "hand, hand_type",
+    (
+        ("A2345", Type.HIGH_CARD),
+        ("AA345", Type.ONE_PAIR),
+        ("AAA45", Type.THREE_OF_A_KIND),
+        ("AAAA5", Type.FOUR_OF_A_KIND),
+        ("AAAAA", Type.FIVE_OF_A_KIND),
+        ("AAA44", Type.FULL_HOUSE),
+        ("32T3K", Type.ONE_PAIR),
+        ("T55J5", Type.THREE_OF_A_KIND),
+        ("KK677", Type.TWO_PAIR),
+        ("KTJJT", Type.TWO_PAIR),
+        ("QQQJA", Type.THREE_OF_A_KIND),
+    )
+)
